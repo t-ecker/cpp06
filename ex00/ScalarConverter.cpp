0017933c3b9045ce394dc2cc4aee35d7888763a1
t-ecker <152566019+t-ecker@users.noreply.github.com>
2024-12-03 15:36:48 +0100

    added second part of ex00

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tecker <tecker@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 14:00:02 by tecker            #+#    #+#             */
+/*   Updated: 2024/12/03 15:26:40 by tecker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ScalarConverter.hpp"
 
 enum values
@@ -32,8 +44,10 @@ int	get_type(const std::string str)
             return INT;
     }
     catch (const std::invalid_argument &e) {}
-    catch (const std::out_of_range &e) {
-		std::cout << "kk\n";
+    catch (const std::out_of_range &e)
+	{
+		std::cout << "input is int out of range\nInput a valid Int" << std::endl;
+		return (-1);
 	}
 
     try
@@ -47,7 +61,9 @@ int	get_type(const std::string str)
 			return FLOAT;
     }
     catch (const std::invalid_argument &e) {}
-    catch (const std::out_of_range &e) {}
+    catch (const std::out_of_range &e) {
+		std::cout << "jj" << std::endl;
+	}
 
     try
 	{
@@ -56,9 +72,62 @@ int	get_type(const std::string str)
         	return DOUBLE;
     }
     catch (const std::invalid_argument &e) {}
-    catch (const std::out_of_range &e) {}
+    catch (const std::out_of_range &e) {
+		std::cout << "pp" << std::endl;
+	}
 
     return (-1);
+}
+
+void handle_char(const std::string str)
+{
+	char c = str[0];
+	std::cout << "char: " << c << std::endl;
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << 'f' << std::endl;
+	std::cout << "double: " <<  std::fixed << std::setprecision(1) << static_cast<double>(c) << std::endl;
+}
+
+void handle_int(const std::string str)
+{
+	int i = stoi(str);
+	if (i > 32 && i < 127)
+		std::cout << "char: " << static_cast<char>(i) << std::endl;
+	else
+		std::cout << "char: char not printable" << std::endl;
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(i) << 'f' << std::endl;
+	std::cout << "double: " <<  std::fixed << std::setprecision(1) << static_cast<double>(i) << std::endl;
+}
+
+void handle_float(const std::string str)
+{
+	float f = stof(str);
+	if (f > 32.0 && f < 127.0)
+		std::cout << "char: " << static_cast<char>(f) << std::endl;
+	else
+		std::cout << "char: char not printable" << std::endl;
+	if (!isnan(f) && !isinf(f))
+		std::cout << "int: " << static_cast<int>(f) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << std::fixed << f << 'f' << std::endl;
+	std::cout << "double: " <<  std::fixed << std::setprecision(1) << static_cast<double>(f) << std::endl;
+}
+
+void handle_double(const std::string str)
+{
+	double d = stod(str);
+	if (d > 32.0 && d < 127.0)
+		std::cout << "char: " << static_cast<char>(d) << std::endl;
+	else
+		std::cout << "char: char not printable" << std::endl;
+	if (!isnan(d) && !isinf(d))
+		std::cout << "int: " << static_cast<int>(d) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<double>(d) << 'f' << std::endl;
+	std::cout << "double: " <<  std::fixed << std::setprecision(1) << d << std::endl;
 }
 
 void ScalarConverter::convert(std::string str)
@@ -67,20 +136,20 @@ void ScalarConverter::convert(std::string str)
 	switch(type)
 	{
 		case CHAR:
-			std::cout << "char" << std::endl;;
-			// handle_char(str);
+			std::cout << "input type: char\n" << std::endl;;
+			handle_char(str);
 			break;
 		case INT:
-			std::cout << "int" << std::endl;;
-			// handle_int(str);
+			std::cout << "input type: int\n" << std::endl;;
+			handle_int(str);
 			break;
 		case FLOAT:
-			std::cout << "float" << std::endl;;
-			// handle_float(str);
+			std::cout << "input type: float\n" << std::endl;;
+			handle_float(str);
 			break;
 		case DOUBLE:
-			std::cout << "double" << std::endl;
-			// handle_double(str);
+			std::cout << "input type: double\n" << std::endl;;
+			handle_double(str);
 			break;
 		default:
 			std::cerr << "Wrong Input" << std::endl;;
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,9 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tecker <tecker@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 14:00:06 by tecker            #+#    #+#             */
+/*   Updated: 2024/12/03 15:04:24 by tecker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
 #include <string.h>
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
+#include <climits>
+#include <math.h>
 
 class ScalarConverter
 {
